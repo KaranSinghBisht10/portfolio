19ab7957c7d41aba550db5f08c4c16c7175961b5
Karan Singh Bisht <bishtkaransingh10@gmail.com>
2023-01-29 21:38:51 +0530

    added all files

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+document.querySelector('.symbol').addEventListener("click",  ()=> {
+        document.querySelector('.sidebar').classList.toggle('sidebarGo');
+        
+    }
+
+
+)
+let about=document.getElementById("About");
